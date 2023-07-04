839735e94af81d4f7818f8c130089239c6d4cfb2
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-07-04 21:21:28 +0530

    [Pankaj] Added Nlog problem.

---
+++ NlogDemoApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NLog;
+
+namespace NlogDemoApplication
+{
+    class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Working Directory: {Environment.CurrentDirectory}");
+            NLog.LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration("NLog.config", true);
+
+            try
+            {
+                Logger.Info("Starting the application...");
+
+                // Simulate some code that might throw an exception
+                int result = DivideNumbers(10, 05);
+
+                Logger.Info($"Result: {result}");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex, "An error occurred in the application.");
+            }
+            finally
+            {
+                Logger.Info("Exiting the application...");
+            }
+        }
+
+        static int DivideNumbers(int a, int b)
+        {
+            Logger.Info($"Dividing {a} by {b}");
+            return a / b;
+        }
+    }
+}
